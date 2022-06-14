356e222e4513e5139884b3b26694478d39c9dfe5
Angela Vogler <angelavogler4@gmail.com>
2022-06-13 21:36:43 -0400

    adding under construction page

--- src/components/underConstructionPage/UnderConstructionPage.tsx
+++ src/components/underConstructionPage/UnderConstructionPage.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import styled from 'styled-components'
 
 import fullsizeLogo from '../../assets/logos/wedding-logo-full.svg'
-import collapsedLogo from '../../assets/logos/wedding-logo-collapsed.svg'
+
+import { breakpoints, fontFamily, color } from '../../styles/theme'
 
 
 const UnderConstructionPage: React.FC = () => (
     <PageContainer>
         <FullSizeLogo src={fullsizeLogo} alt="logo"/>
+        <H1>Website Coming Soon</H1>
     </PageContainer>
 )
 
@@ -15,10 +17,26 @@ const PageContainer = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
+    flex-direction: column;
+    margin: 48px 16px;
+
+    @media only screen and (min-width: ${breakpoints.small}) {
+        margin: 48px;
+      }
 `
 
 const FullSizeLogo = styled.img`
+    width: 350px;
 
+    @media only screen and (min-width: ${breakpoints.small}) {
+        height: 450px;
+        width: 100%;
+      }
+`
+
+const H1 = styled.h1`
+    font-family: ${fontFamily.playfairDisplaySC};
+    color: ${color.mainPalette.green80};
 `
 
 export default UnderConstructionPage
