e33fcf971bd7eb43542f3ecb3ed6a58c953a7b5a
Angela Vogler <angelavogler4@gmail.com>
2022-07-10 18:58:22 -0400

    working on drawer animation

--- src/components/drawer/Drawer.tsx
+++ src/components/drawer/Drawer.tsx
@@ -45,29 +45,16 @@ const StyledDrawer = styled.div<Partial<DrawerProps>>`
   z-index: 1500;
   height: 100%;
   max-width: 100%;
-  transition: transform 250ms ease-in-out;
+  transition: transform 300ms ease-in-out;
   background: ${color.mainPalette.neutral0};
   width: 325px;
-  box-shadow: ${boxShadow.downward.medium};
   top: unset;
   bottom: 0;
 
   ${({ open }) => `
-    transform: ${open ? 'none' : `translateY(100%)`};
+    transform: ${open ? 'none' : `translateX(-100%)`};
+    box-shadow: ${open ? `${boxShadow.downward.medium}` : 'none'};
   `}
-
-  @media screen and (min-width: ${breakpoints.medium}) {
-    top: 0;
-
-    ${({ open }) => `
-      left: 0;
-
-      transform: ${
-        // TODO: fix this animation
-        open ? 'none' : `translateX(-110%)`
-      };
-    `}
-  }
 `
 
 export default Drawer
