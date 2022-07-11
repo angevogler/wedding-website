11decb301b3346002c8eb4eea979c32525a24fc9
Angela Vogler <angelavogler4@gmail.com>
2022-07-10 22:33:06 -0400

    adding q+a objects

---
+++ src/components/faqPage/questionsAndAnswers.ts
@@ -0,0 +1,47 @@
+export const questionsAndAnswers = [
+    {
+        index: 0,
+        question: "When should I RSVP by?",
+        answer: "We will send out formal invitations closer to the big day, however if you’ve received a save the date and know you can’t attend please let us know",
+    },
+    {
+        index: 1,
+        question: "Are kids welcome?",
+        answer: "We love your kids, but since our venue is a brewery please enjoy this as a parents’ night out!"
+    },
+    {
+        index: 2,
+        question: "Do you have any hotel recommendations?",
+        answer: "We have room blocks at the DoubleTree Biltmore Village and Hampton Inn Biltmore Village, which share a parking lot. Our transportation will also be leaving from this location and dropping you back off here at the end of the night",
+    },
+    {
+        index: 3,
+        question: "What should I wear?",
+        answer: "Cocktail attire is requested. Suits or sports coats for men (tie optional), and cocktail dresses or jumpsuits for women. We respectfully ask that you do not wear colors traditionally reserved for the bride such as whites, ivories, or creams. Since our wedding is taking place during the spring, we encourage guests to wear pastels or brighter colors",
+    },
+    {
+        index: 4,
+        question: "Will the ceremony and reception be indoors or outdoors?",
+        answer: "Weather permitting our ceremony and cocktail hour will both take place outdoors on the rooftop. We will head inside afterwards for dinner and dancing at the reception. In the event of rain, the ceremony and cocktail hour will also be moved indoors",
+    },
+    {
+        index: 5,
+        question: "Can I take pictures at your wedding?",
+        answer: "We are having an unplugged ceremony and kindly ask that you leave your phones off and away during the ceremony. Our photographers are amazing and we want to make sure that they are able to capture your lovely faces during the ceremony, instead of having them hidden behind electronics. Once the ceremony has ended you are welcome to take your phones back out for the rest of the night!",
+    },
+    {
+        index: 6,
+        question: "What time should I arrive?",
+        answer: "The ceremony starts at 5:30pm and the venue will open up for our guests at 5:00pm. We kindly ask that you are seated and ready to begin on time",
+    },
+    {
+        index: 7,
+        question: "Will there be transportation provided?",
+        answer: "Yes - we will be providing transportation from the DoubleTree Biltmore Village. There will be more information to follow",
+    },
+    // {
+    //     index: 8,
+    //     question: "Is there parking available?",
+    //     answer: "",
+    // },
+]
