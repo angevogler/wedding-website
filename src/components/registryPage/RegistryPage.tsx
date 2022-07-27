79f2168b2c617dd19e0782cf52cc110150c94151
Angela Vogler <angelavogler4@gmail.com>
2022-07-26 20:55:00 -0400

    styling registry message

--- src/components/registryPage/RegistryPage.tsx
+++ src/components/registryPage/RegistryPage.tsx
@@ -1,15 +1,49 @@
 import React from 'react'
 import styled from 'styled-components'
+
+import { color, breakpoints } from '../../styles/theme'
 import { BodyLarge } from '../typography'
 
 const RegistryPage: React.FC = () => (
     <RegistryContainer>
-        <BodyLarge>
-        Your love, laughter and company on our wedding day is the greatest gift of all. However, should you wish to help us celebrate with a gift, you can find our registry here.
-        </BodyLarge>
+        <RegistryMessage>
+            <BodyLarge>
+            Your love, laughter, and company on our wedding day is the greatest gift of all. However, should you wish to help us celebrate with a gift, you can find our registry here.
+            </BodyLarge>
+        </RegistryMessage>
     </RegistryContainer>
 )
 
-const RegistryContainer = styled.div``
+const RegistryContainer = styled.div`
+    border: 1px solid ${color.mainPalette.green80};
+    padding: 16px 8px;
+    display: flex;
+    flex-direction: column;
+    max-width: 800px;
+    margin-left: auto;
+    margin-right: auto;
+    background-color: ${color.mainPalette.green40};
+    margin-top: 24px;
+
+    @media only screen and (min-width: ${breakpoints.small}) {
+        padding: 24px 16px;
+    }
+`
+
+const RegistryMessage = styled.div`
+    border: 1px solid ${color.mainPalette.green80};
+    padding: 16px 8px;
+    display: flex;
+    flex-direction: column;
+    max-width: 800px;
+    margin-left: auto
+    margin-right: auto;
+    background-color: ${color.mainPalette.green20};
+
+    @media only screen and (min-width: ${breakpoints.small}) {
+        padding: 24px 16px;
+        margin: 8px 32px;
+    }
+`
 
 export default RegistryPage
