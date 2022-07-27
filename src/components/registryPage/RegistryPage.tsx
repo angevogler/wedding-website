e44e525eaddfb802505262f87e1a8f130ada7304
Angela Vogler <angelavogler4@gmail.com>
2022-07-26 21:45:28 -0400

    adding registry iframe

--- src/components/registryPage/RegistryPage.tsx
+++ src/components/registryPage/RegistryPage.tsx
@@ -6,25 +6,35 @@ import { BodyLarge } from '../typography'
 import { Hyperlink } from '../hyperlink'
 
 const RegistryPage: React.FC = () => (
-    <RegistryContainer>
-        <RegistryMessage>
-            <BodyLarge>
-            Your love, laughter, and company on our wedding day 
-            is the greatest gift of all. However, should you wish 
-            to help us celebrate with a gift, you can find our registry
-            {" "}
-            <Hyperlink
-                id="registry-link"
-                href="https://registry.theknot.com/angela-vogler-david-pecunia-april-2023-nc/47901253"
-                target="blank"
-            >
-                here
-            </Hyperlink>
-            .
-            </BodyLarge>
-        </RegistryMessage>
-    </RegistryContainer>
+    <RegistryPageRoot>
+        <RegistryContainer>
+            <RegistryMessage>
+                <BodyLarge>
+                Your love, laughter, and company on our wedding day 
+                is the greatest gift of all. However, should you wish 
+                to help us celebrate with a gift, you can find our registry
+                {" "}
+                <Hyperlink
+                    id="registry-link"
+                    href="https://registry.theknot.com/angela-vogler-david-pecunia-april-2023-nc/47901253"
+                    target="blank"
+                >
+                    here
+                </Hyperlink>
+                .
+                </BodyLarge>
+            </RegistryMessage>
+        </RegistryContainer>
+        <RegistryIframe
+            id="registry-iframe"
+            src="https://registry.theknot.com/angela-vogler-david-pecunia-april-2023-nc/47901253"
+        />
+    </RegistryPageRoot>
 )
+
+const RegistryPageRoot = styled.div`
+    height: calc(100vh - (234px + 72px + 24px));
+`
 
 const RegistryContainer = styled.div`
     border: 1px solid ${color.mainPalette.green80};
@@ -38,7 +48,7 @@ const RegistryContainer = styled.div`
     margin-top: 24px;
 
     @media only screen and (min-width: ${breakpoints.small}) {
-        padding: 24px 16px;
+        display: none;
     }
 `
 
@@ -55,6 +65,17 @@ const RegistryMessage = styled.div`
     @media only screen and (min-width: ${breakpoints.small}) {
         padding: 24px 16px;
         margin: 8px 32px;
+    }
+`
+
+const RegistryIframe = styled.iframe`
+    display: none;
+
+    @media only screen and (min-width: ${breakpoints.small}) {
+        display: block;
+        height: 100%;
+        width: 100%;
+        border: 1px solid ${color.mainPalette.green40};
     }
 `
 
